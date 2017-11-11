9f135105ebd5d421adb11d328f485b8cb5eb0f06
Ayush Verma <ayushverma@localhost.localdomain>
2017-11-11 12:13:21 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var jsonServer = require('json-server');
+
+var server = jsonServer.create(); // Returns an Express server
+var router = jsonServer.router('engati.json'); // Returns an Express router
+
+server.use(jsonServer.defaults); // logger, static and cors middlewares
+server.use(router); // Mount router on '/'
+
+server.listen(3000);
